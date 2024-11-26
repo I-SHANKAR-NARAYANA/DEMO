1543640f55b854ac099ac1e282fa7a63e45b5733
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2024-11-26 21:34:05 +0530

    Babbel Scrippts Fail

--- babbel/m.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import os
-c = urllib.request.urlopen("https://i-shankar-narayana.github.io/DEMO/babbel/11.txt").read().decode('utf-8')
-d = ''.join([chr( (ord(x) - 24 ) % 256) for x in c])
-# cmd = 'echo | set /p nul=' + d.strip() + '| clip'
-# os.system(cmd)
-print(d)
